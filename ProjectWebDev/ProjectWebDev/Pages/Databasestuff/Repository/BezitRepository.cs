87abfeee73272caff787cba75ffcb6e8dbdc29ad
Jelle5 <91126597+Jelle5@users.noreply.github.com>
2022-03-23 14:56:39 +0100

    mijn shit

--- ProjectWebDev/ProjectWebDev/Pages/Databasestuff/Repository/BezitRepository.cs
+++ ProjectWebDev/ProjectWebDev/Pages/Databasestuff/Repository/BezitRepository.cs
@@ -21,11 +21,21 @@ public class BezitRepository
         connection.Query(sql, new {strip_id});
     }
 
-    public void GetNotes(int strip_id)
+    public string GetNotes(int strip_id, int gebruiker_id)
     {
-        string sql = @"SELECT notes FROM Bezit WHERE Strip_id = @strip_id";
+        string sql = @"SELECT notes FROM Bezit WHERE Strip_id = @strip_id AND gebruiker_id = @gebruiker_id";
 
         using var connection = GetConnection();
-        connection.Query(sql, new {strip_id});
+       string notes = connection.ExecuteScalar<string>(sql, new {strip_id, gebruiker_id});
+       return notes;
+    }
+    
+    public string UpdateNotes(int strip_id, int gebruiker_id, string Notes)
+    {
+        string sql = @"UPDATE Bezit SET notes = @Notes WHERE Strip_id = @strip_id AND gebruiker_id = @gebruiker_id";
+
+        using var connection = GetConnection();
+        string notes = connection.ExecuteScalar<string>(sql, new {strip_id, gebruiker_id, Notes});
+        return notes;
     }
 }
--- ProjectWebDev/ProjectWebDev/Pages/Overzichten/AddNotes.cshtml.cs
+++ ProjectWebDev/ProjectWebDev/Pages/Overzichten/AddNotes.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjectWebDev.Helpers;
 using ProjectWebDev.Pages.Databasestuff.Models;
 using ProjectWebDev.Pages.Databasestuff.Repository;
 
@@ -8,16 +9,21 @@ namespace ProjectWebDev.Pages.Overzichten;
 
 public class AddNotes : PageModel
 {
-    public IEnumerable<Bezit> Notes { get; set; }
+    public string Notes { get; set; }
     
     public void OnGet([FromQuery] int strip_id)
     {
-        BezitRepository Notes = new BezitRepository();
-        Notes.GetNotes(strip_id);
+        int gebruiker_id = Convert.ToInt32(HttpContext.Session.GetString(SessionConstant.Gebruiker_ID));
+        
+        Notes = new BezitRepository().GetNotes(strip_id, gebruiker_id);
     }
 
-    public IActionResult OnPostToevoegen()
+    public IActionResult OnPostToevoegen([FromQuery] int strip_id, string Notities)
     {
+        string note = Request.Form["Text1"];
+        int gebruiker_id = Convert.ToInt32(HttpContext.Session.GetString(SessionConstant.Gebruiker_ID));
+        Notes = new BezitRepository().UpdateNotes(1, gebruiker_id, note);
+        
         return RedirectToPage("/Overzichten/MyCollection");
     }
 }
