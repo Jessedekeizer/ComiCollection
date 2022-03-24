a49708dd9f626fe2e14ce6e3111f508bcc7f9282
Jelle5 <91126597+Jelle5@users.noreply.github.com>
2022-03-24 11:21:33 +0100

    Otto

---
+++ ProjectWebDev/ProjectWebDev/Pages/Databasestuff/Models/KleurenSchema.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectWebDev.Pages.Databasestuff.Models;
+
+public class KleurenSchema
+{
+    public string navBackground { get; set; } = "#b2acfa";
+    public string navButton { get; set; } = "#6558f5";
+    public string active { get; set; } = "#730fc3";
+    public string Achtergrond { get; set; } = "#92E6D0";
+    public string Background { get; set; } = "#FFFFFF";
+}
